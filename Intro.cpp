8af08dcd03c6cd02d7335466591e43d42eac1ac0
Jasjot Singh <jasjot784@gmail.com>
2019-09-12 15:46:08 +0530

    Introduction(add 2 numbers)

---
+++ Intro.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void sum(int,int);
+int main()
+{
+int a,b;
+cout<<"enter 2 numbers"<<endl;
+cin>>a>>b;
+sum(a,b);
+}
+void sum(int a,int b)
+{
+  cout<<"sum is "<<a+b<<endl;
+
+}
